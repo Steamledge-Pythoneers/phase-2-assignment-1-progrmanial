f04124e75d3615f6041afc730e25188fbba49db1
Saadullah Olawale IMRAN <progrmanial@gmail.com>
2020-09-29 12:33:44 +0100

    Remove redundant lines of code

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -16,30 +16,34 @@ def lowest_terms(x):
 
 	# generate unique set of common factor of the numerator and the denominator
 
-	common_factor = set(factors(numbers[0])) & set(factors(numbers[1]))
+	highest_common_factor = max(set(factors(numbers[0])) & set(factors(numbers[1])))
+	return "{}/{}".format(int(numbers[0]) // highest_common_factor, int(numbers[1]) // highest_common_factor )
 
-	# check for common factors and get the highest common factor
-	# divide the give fractions with highest common factor and return the results
-
-	if common_factor:
-		highest_common_factor = max(common_factor)
-		return "{}/{}".format(int(numbers[0]) // highest_common_factor, int(numbers[1]) // highest_common_factor )
-
-	# if nothing is found about the numbers, return them as the are given
-
-	return "{}/{}".format(numbers[0], numbers[1])
 
 
 
 def factors(x):
-	
+	"""
 	# ###########################################################################
 	# A function that generate the factors of any number
 	# It accept a number
 	# Returns a list of factors or empty list if there are no factors 
+	#
+	# input: x
+	# return: list of factors of x
 	# ###########################################################################
-	
+	"""
+
 	# converts the given number to integer
 	num = abs(int(x))
 	return [i for i in range(1, num+1) if not num % i]
 
+
+print(lowest_terms("20/10"))
+print(lowest_terms("16/28"))
+print(lowest_terms("-50/25") )
+print(lowest_terms("20/-60") )
+print(lowest_terms("-300/165") )
+print(lowest_terms("-12/-26"))
+print(lowest_terms("90/0") )
+print(lowest_terms("0/12") )

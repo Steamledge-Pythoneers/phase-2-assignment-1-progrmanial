9e345bbb5596ca9e8a31cf4ef59d7c1e70f3ba9a
Saadullah Olawale IMRAN <progrmanial@gmail.com>
2020-10-01 17:53:29 +0100

    Update for 0/0 use case

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,25 +1,27 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	if x == "0/0" or x.split('/')[1] == "0": return "Undefined"
-	
-	# Check for zero denominator and zero numerator
+	try:
+		if x == "0/0" or x.split('/')[1] == "0": return "Undefined"
+		
+		# Check for zero denominator and zero numerator
 
-	numbers = x.split("/")
-	if numbers[0] == "0": return "0"
+		numbers = x.split("/")
+		if numbers[0] == "0": return "0"
 
-	# converts negative denominator to positive and transfer it to numerator
-	# eliminate negative sign if both the numerator and denominator are negative
+		# converts negative denominator to positive and transfer it to numerator
+		# eliminate negative sign if both the numerator and denominator are negative
 
-	if (int(numbers[1]) < 0):
-		numbers = [-1 * int(x) for x in numbers]
+		if (int(numbers[1]) < 0):
+			numbers = [-1 * int(x) for x in numbers]
 
-	# generate unique set of common factor of the numerator and the denominator
+		# generate unique set of common factor of the numerator and the denominator
 
-	highest_common_factor = max(set(factors(numbers[0])) & set(factors(numbers[1])))
-	return "{}/{}".format(int(numbers[0]) // highest_common_factor, int(numbers[1]) // highest_common_factor )
+		highest_common_factor = max(set(factors(numbers[0])) & set(factors(numbers[1])))
+		return "{}/{}".format(int(numbers[0]) // highest_common_factor, int(numbers[1]) // highest_common_factor )
 
-
+	except Exception as e:
+		print(e, "check your input and try again")
 
 
 def factors(x):
